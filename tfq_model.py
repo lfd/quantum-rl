0a3195d9ef95b04d69c3b1da0d8c6b1ab38cf3b8
majafranz <majafranz99@gmail.com>
2021-05-18 13:34:51 +0200

    Added new config without full parameterization

--- tfq_model.py
+++ tfq_model.py
@@ -29,10 +29,93 @@ class QVC_Model(keras.Model):
     def create_circuit(self, num_qubits, num_layers):
         circuit = cirq.Circuit()
 
+        symbols = sympy.symbols('param0:' + str(num_qubits*num_layers + 2*2*3 )) # qubits * layers + 2 * 2 * 3 pooling
+
+        for idx in range(num_layers):
+            circuit += self.vqc_layer(symbols=symbols[idx*num_qubits : (idx+1)*num_qubits])
+
+        circuit += self.pool(source=self.qubits[0], sink=self.qubits[2], 
+                            symbols=symbols[num_qubits*num_layers : num_qubits*num_layers+6])
+        circuit += self.pool(source=self.qubits[1], sink=self.qubits[3], 
+                            symbols=symbols[num_qubits*num_layers+6 : ])
+
+        return circuit
+
+
+    def vqc_layer(self, symbols, nonlinearity=None):
+        
+        symbols = self.reparameterize(symbols, nonlinearity)
+
+        circuit = cirq.Circuit()
+
+        # Create entanglement
+        for idx in range(1, len(self.qubits)):
+            circuit.append(cirq.CNOT(control=self.qubits[idx], target=self.qubits[idx-1]))
+
+        # Apply qubit rotations
+        for idx in range(len(self.qubits)):
+            circuit.append([cirq.rx(symbols[idx]).on(self.qubits[idx]), 
+                            cirq.ry(0.5 * np.pi).on(self.qubits[idx]), 
+                            cirq.rz(np.pi).on(self.qubits[idx])])
+
+        return circuit
+
+
+    # using pooling as in reference implementation
+    def pool(self, source, sink, symbols, nonlinearity=None):
+        symbols = self.reparameterize(symbols, nonlinearity)
+        
+        circuit = cirq.Circuit()
+
+        circuit.append([cirq.rx(symbols[0]).on(source), 
+                        cirq.ry(symbols[1]).on(source), 
+                        cirq.rz(symbols[2]).on(source)])
+
+        circuit.append([cirq.rx(symbols[3]).on(sink), 
+                        cirq.ry(symbols[4]).on(sink), 
+                        cirq.rz(symbols[5]).on(sink)])
+
+        circuit.append(cirq.CNOT(control=source, target=sink))
+
+        circuit.append([cirq.rx(-symbols[0]).on(sink), 
+                        cirq.ry(-symbols[1]).on(sink), 
+                        cirq.rz(-symbols[2]).on(sink)])
+        return circuit
+
+    def encode_data(self, input, asTensor=True):
+        circuit = cirq.Circuit()
+        for i, angle in enumerate(input):
+            angle = angle.numpy()
+            circuit.append(cirq.rx(angle).on(self.qubits[i]))
+            circuit.append(cirq.rz(angle).on(self.qubits[i]))
+        if asTensor:
+            return tfq.convert_to_tensor([circuit])
+        else:
+            return circuit
+
+    def reparameterize(self, weight_symbols, nonlinearity=None):
+        
+        # TODO add nonlinearity
+        
+        if nonlinearity == None:
+            weight_symbols = [symbol * 2 * np.pi for symbol in weight_symbols]
+
+        return weight_symbols
+
+
+# full parameterized QVC as in reference implementation (https://github.com/lockwo/quantum_computation)
+class QVC_Model_full_parameterized(QVC_Model):
+    def __init__(self,  num_qubits, num_layers, nonlinearity=None):
+        super(QVC_Model_full_parameterized, self).__init__(num_qubits, num_layers, nonlinearity)
+
+
+    def create_circuit(self, num_qubits, num_layers):
+        circuit = cirq.Circuit()
+
         symbols = sympy.symbols('param0:' + str(num_qubits*num_layers*3 + 2*3*3)) # qubits * layers * 3 weights + 2 * 3 * 3 pooling
 
         for idx in range(num_layers):
-            circuit += self.vqc_layer(symbols=symbols[idx*num_qubits*num_layers : (idx+1)*num_qubits*num_layers])
+            circuit += self.vqc_layer(symbols=symbols[idx*num_qubits*3 : (idx+1)*num_qubits*3])
 
         circuit += self.pool(source=self.qubits[0], sink=self.qubits[2], 
                             symbols=symbols[num_qubits*num_layers*3 : num_qubits*num_layers*3+9])
@@ -79,26 +162,4 @@ class QVC_Model(keras.Model):
         circuit.append([cirq.rx(-symbols[6]).on(sink), 
                         cirq.ry(-symbols[7]).on(sink), 
                         cirq.rz(-symbols[8]).on(sink)])
-
         return circuit
-
-    def encode_data(self, input, asTensor=True):
-        circuit = cirq.Circuit()
-        for i, angle in enumerate(input):
-            angle = angle.numpy()
-            circuit.append(cirq.rx(angle).on(self.qubits[i]))
-            circuit.append(cirq.rz(angle).on(self.qubits[i]))
-        if asTensor:
-            return tfq.convert_to_tensor([circuit])
-        else:
-            return circuit
-
-    def reparameterize(self, weight_symbols, nonlinearity=None):
-        
-        # TODO add nonlinearity
-        
-        if nonlinearity == None:
-            weight_symbols = [symbol * 2 * np.pi for symbol in weight_symbols]
-
-        return weight_symbols
-
